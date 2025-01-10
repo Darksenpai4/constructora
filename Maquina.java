4011eba9b0114b4aee3f075a9c91a06af3759c3f
Carlos Manuel <carlosm.matros@educa.jcyl.es>
2025-01-10 10:18:23 +0100

    Commit 2: Segunda parte de la tercera clase (colaborador)

--- Maquina.java
+++ Maquina.java
@@ -1,26 +1,67 @@
-class Maquina{
+class Maquina {
     private String nombreMaquina;
-    private boolean funciona; 
+    private boolean funciona;
 
-    /** 
+    /**
      * Constructor de la clase Agenda
      */
-    public Maquina(String nombre){
+    public Maquina(String nombre) {
         nombreMaquina = nombre;
         funciona = true;
     }
 
-    /** 
+    /**
      * Obtener el nombre de la máquina
      */
-    public String getNombre(){
+    public String getNombre() {
         return nombreMaquina;
     }
 
-    /** 
+    /**
      * Obtener si funciona la máquina
      */
-    public boolean getFuncionamiento(){
+    public boolean getFuncionamiento() {
         return funciona;
+    }
+
+    /**
+     * Modificar el nombre de la máquina
+     */
+    public void setNombre(String nombre) {
+        nombreMaquina = nombre;
+    }
+
+    /**
+     * Modificar el estado de la máquina
+     */
+    public void setFuncionamiento() {
+        funciona = !funciona;
+    }
+
+    /**
+     * Imprimir detalles
+     */
+    public void imprimirDetalles() {
+        System.out.println("Máquina: "+nombreMaquina);
+        if (funciona) {
+            System.out.println("Funciona la máquina:Si");
+        } else {
+            System.out.println("Funciona la máquina: No");
+        }
+    }
+
+    /**
+     * Obtener detalles
+     */
+    public String getDetalles(){
+        String funcion;
+        if (funciona) {
+            funcion = "Si";
+        } else {
+            funcion = "No";
+        }
+
+
+        return "Máquina: " + nombreMaquina  + " | Funciona la máquina: " +funcion;
     }
 }
